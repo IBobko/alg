e49b6a4c461ec64ffe6fbadd506ae65c67b78d41
Igor Bobko <iibobko@gmail.com>
2023-02-04 01:32:03 -0800

    update #1

--- main.cpp
+++ main.cpp
@@ -1,73 +1,49 @@
 #include <iostream>
 
 struct State {
-    bool matched;
-    State *FALSE = nullptr;
-    State *TRUE = nullptr;
-    std::string msg;
+    bool match;
 };
 
-bool isCorrect1(const bool param[], int index) {
-    bool correct = true;
+
+bool isCorrect(State *s, const bool param[], int index) {
+    bool correct = s->match;
     if (param[index] == correct) {
         if (index + 1 < 5) {
-            return isCorrect1(param, index + 1);
+            return isCorrect(s, param, index + 1);
         }
         return true;
     } else {
         return false;
     }
 }
-
-bool isCorrect2(const bool param[], int index) {
-    bool correct = false;
-    if (param[index] == correct) {
-        if (index + 1 < 5) {
-            return isCorrect2(param, index + 1);
-        }
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-/*bool traverse(State * t, bool p[],index) {
-    if (t->matched == p[0])
-    if (p) {
-        if (t->TRUE!= nullptr) {
-            traverse(t->TRUE, p);
-        }
-    } else {
-        if (t->FALSE!= nullptr) {
-            traverse(t->FALSE, p);
-        }
-    }
-    std::cout << t->msg << std::endl;
-}
- */
 
 int main() {
-    bool *inputs = new bool[5];
-    inputs[0] = true;
-    inputs[1] = true;
-    inputs[2] = true;
-    inputs[3] = true;
-    inputs[4] = true;
+    State T;
+    T.match = true;
 
-    bool *inputs2 = new bool[5];
-    inputs2[0] = false;
-    inputs2[1] = false;
-    inputs2[2] = false;
-    inputs2[3] = false;
-    inputs2[4] = false;
+    State F;
+    F.match = false;
 
-    if (isCorrect1(inputs, 0)) {
-        std::cout << "Полет нормальный" << std::endl;
+    bool *inputsTrue = new bool[5];
+    inputsTrue[0] = true;
+    inputsTrue[1] = true;
+    inputsTrue[2] = true;
+    inputsTrue[3] = true;
+    inputsTrue[4] = true;
+
+    bool *inputsFalse = new bool[5];
+    inputsFalse[0] = false;
+    inputsFalse[1] = false;
+    inputsFalse[2] = false;
+    inputsFalse[3] = false;
+    inputsFalse[4] = false;
+
+    if (isCorrect(&T, inputsTrue, 0)) {
+        std::cout << "Result: true+" << std::endl;
     }
 
-    if (isCorrect2(inputs2, 0)) {
-        std::cout << "Полет ненормальный" << std::endl;
+    if (isCorrect(&F, inputsFalse, 0)) {
+        std::cout << "Result: false+" << std::endl;
     }
 
 
