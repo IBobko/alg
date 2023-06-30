9a96180e3ec37a66ab47f4081ccd88e1c04f3e7f
Igor Bobko <iibobko@gmail.com>
2023-06-29 22:25:58 -0700

    Refactor google/main.py and add command line argument support
    This commit refactors the `google/main.py` file and adds support for command line arguments using the `argparse` module. The script can now be executed with different commands, such as `save_contacts` and `other_command`. The appropriate actions are taken based on the command provided. This enhances the flexibility and extensibility of the script.
    
    Changes made:
    - Added `argparse` import
    - Defined a command line argument parser
    - Implemented logic to execute different commands
    
    Please review and merge this commit.

--- google/main.py
+++ google/main.py
@@ -1,13 +1,29 @@
+import argparse
+
 from contact_service import ContactsService
-from google_sheets import GoogleSheets
 from google_cloud import GoogleCloud
+from google_sheets import GoogleSheets
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Script for performing various operations.')
+    parser.add_argument('command', choices=['save_contacts', 'other_command'], help='The command to execute')
+
+    args = parser.parse_args()
+    command = args.command
+
     google_cloud = GoogleCloud()
     contacts_service = ContactsService(google_cloud)
     google_sheets = GoogleSheets(google_cloud, contacts_service)
-    google_sheets.save_contacts_to_sheet()
+
+    if command == "save_contacts":
+        print("save_contacts:", command)
+        google_sheets.save_contacts_to_sheet()
+    elif command == "other_command":
+        print("other_command:", command)
+        pass
+    else:
+        print("Unknown command:", command)
 
 
 if __name__ == '__main__':
